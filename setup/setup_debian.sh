d4721601d4c908d1fb6dd578891f32251455632d
BenjamenMeyer <ben.meyer@rackspace.com>
2014-06-26 21:41:23 +0000

    Refactor: Order
    - refactored order of functions so functions are used after they are declared and a little better organized
    - Added function to get the LDAP Server Address
    - Added functionality to configure an LDAP Client System to use the LDAP Server

--- setup/setup_debian.sh
+++ setup/setup_debian.sh
@@ -25,6 +25,7 @@
 
     LDAP_DC=""
     LDAP_ADMIN=""
+    LDAP_SERVER_ADDRESS=""
 
     TLS_CACERT=""
     TLS_SERVER_PRIVATE_KEY_FILE=""
@@ -37,7 +38,6 @@
 #####################
 # Utility Functions #
 #####################
-
 
 # Check if the user is a root-level user
 # This can be achieved by using 'sudo'
@@ -57,7 +57,7 @@ function is_user_root()
 function get_ldap_dc()
     {
     if [ -z "${LDAP_DC}" ]; then
-        read -p "Please enter the LDAP DC:" LDAP_DC
+        read -p "Please enter the LDAP DC: " LDAP_DC
     fi
 
     if [ -n "${LDAP_DC}" ]; then
@@ -77,7 +77,7 @@ function get_ldap_admin()
     fi
 
     if [ -z "${LDAP_ADMIN}" ]; then
-        read -p "Please enter the Admin User Name for ${LDAP_DC} (e.g cn=admin):" LDAP_ADMIN
+        read -p "Please enter the Admin User Name for ${LDAP_DC} (e.g cn=admin): " LDAP_ADMIN
     fi
 
     if [ -n "${LDAP_ADMIN}" ]; then
@@ -88,20 +88,173 @@ function get_ldap_admin()
     fi
     }
 
+# Tell user how to configure a portion of the LDAP Configuration to enable LDAPS how they want
+function warn_user_config_update_ldaps()
+    {
+    if [ -f /etc/debian_version]; then
+        echo
+        echo WARNING: You must enable LDAPS in the /etc/default/slapd file.
+        echo    To do so, change the SLAPD_SERVICES to specify LDAPS.
+        echo    For example:
+        echo
+        echo        SLAPD_SERVICES="ldap:/// ldapi:///"
+        echo
+        echo    becomes:
+        echo
+        echo        SLAPD_SERVICES="ldap://127.0.0.1:389/ ldaps:/// ldapi:///"
+        echo
+        echo    with non-TLS LDAP remaining available on the local-host
+        echo
+    fi
+    }
+
+# Retrieve the LDAP Server Address from the user
+function get_ldap_server_address()
+    {
+    local IFS_OLD=${IFS}
+    local PS3_OLD=${PS3}
+
+    PS3="Is the LDAP Server remote? "
+    IFS='
+'
+    OPTIONS="Yes
+No"
+    select OPTION in ${OPTIONS}
+    do
+        case "${OPTION}" in
+            "Yes")
+                read -p "Please enter the server address: " LDAP_SERVER_ADDRESS
+                local PS3_MAIN=${PS3}
+                local TERMINATE="No"
+                PS3="Is ${LDAP_SERVER_ADDRESS} correct? "
+                select TERMINATE in ${OPTIONS}
+                do
+                    case "${OPTION}" in
+                        "Yes")
+                            break
+                            ;;
+
+                         "No")
+                            break
+                            ;;
+                    esac
+                done
+
+                PS3=${PS3_MAIN}
+
+                if [ "${TERMINATE}" == "Yes" ]; then
+                    break
+                fi
+                ;;
+
+            "No")
+                LDAP_SERVER_ADDRESS="127.0.0.1"
+                break
+                ;;
+        esac
+    done
+
+    echo "LDAP Server Address: ${LDAP_SERVER_ADDRESS}"
+
+    PS3=${PS3_OLD}
+    IFS=${IFS_OLD}
+
+    if [ -n "${LDAP_SERVER_ADDRESS}" ]; then
+        return 1
+    else
+        return 0
+    fi
+    }
+
+# Get the TLS Certificate information
+# Takes 1 parameter:
+#   CLIENT_ONLY: true/false
+#       If true, then only prompts for the CA Cert
+#       If false, prompts for the CA Cert, Server Private Key, and Server Certificate
+function get_tls_certs()
+    {
+    local CLIENT_ONLY=${1}
+
+    if [ -z "${TLS_CACERT}" ]; then
+        read -p "Please enter the full path to the CA's PEM file: " TLS_CACERT
+    fi
+
+    if [ ${CLIENT_ONLY} ]; then
+
+        if [ -n "${TLS_CACERT}" ]; then
+            return 1
+        else
+            echo "Failed to get Certificate Authority's Key"
+            return 0
+        fi
+
+    else
+
+        if [ -z "${TLS_SERVER_PRIVATE_KEY_FILE}" ]; then
+            read -p "Please enter the Server's Private Key PEM file: " TLS_SERVER_PRIVATE_KEY_FILE
+        fi
+
+        if [ -z "${TLS_SERVER_CERT_FILE}" ]; then
+            read -p "Please enter the Server's Signed Key PEM (CRT) file: " TLS_SERVER_CERT_FILE
+        fi
+
+        local IFS_OLD=${IFS}
+        local PS3_OLD=${PS3}
+
+        IFS='
+    '
+        OPTIONS="Yes
+No"
+        PS3="Are the certificates self-signed? "
+        select TLS_SELF_SIGNED in ${OPTIONS}
+        do
+            case "${TLS_SELF_SIGNED}" in
+                "Yes")
+                    break
+                    ;;
+                "No")
+                    break
+                    ;;
+            esac
+        done
+
+        PS3=${PS3_OLD}
+        IFS=${OLD_IFS}
+
+        if [ -n "${TLS_CACERT}" ]; then
+            if [ -n "${TLS_SERVER_PRIVATE_KEY_FILE}" ]; then
+                if [ -n "${TLS_SERVER_CERT_FILE}" ]; then
+                    return 1
+                else
+                    echo "Failed to get Server's Signed Key"
+                    return 0
+                fi
+            else
+                echo "Failed to get Server's Private Key"
+                return 0
+            fi
+        else
+            echo "Failed to get Certificate Authority's Key"
+            return 0
+        fi
+
+    fi
+    }
+
 # Generate GNU TLS Certs
 function generate_tls_certs()
     {
-    get_tls_certs
+    get_tls_certs false
     if [ $? -eq 1 ]; then
         local IFS_OLD=${IFS}
         local PS3_OLD=${PS3}
 
-        PS3="Were the certificates issued by OpenSSL?"
+        PS3="Were the certificates issued by OpenSSL? "
         IFS='
 '
-        FOOBAR_OPTIONS="Yes
+        OPTIONS="Yes
 No"
-        select OPTION in ${FOOBAR_OPTIONS}
+        select OPTION in ${OPTIONS}
         do
             case "${OPTION}" in
                 "Yes")
@@ -125,12 +278,9 @@ No"
                 "No")
                     break
                     ;;
-                *)
-                    # Re-print the menu?
-                    ;;
             esac
         done
-        
+
         PS3=${PS3_OLD}
         IFS=${IFS_OLD}
 
@@ -141,86 +291,98 @@ No"
     fi
     }
 
-# Get the TLS Certificate information
-function get_tls_certs()
+# Add access control
+function add_access_control()
     {
-    if [ -z "${TLS_CACERT}" ]; then
-        read -p "Please enter the full path to the CA's PEM file:" TLS_CACERT
-    fi
-
-    if [ -z "${TLS_SERVER_PRIVATE_KEY_FILE}" ]; then
-        read -p "Please enter the Server's Private Key PEM file:" TLS_SERVER_PRIVATE_KEY_FILE
-    fi
-
-    if [ -z "${TLS_SERVER_CERT_FILE}" ]; then
-        read -p "Please enter the Server's Signed Key PEM (CRT) file:" TLS_SERVER_CERT_FILE
-    fi
-    
-    local IFS_OLD=${IFS}
-    local PS3_OLD=${PS3}
-
-    IFS='
-'
-    OPTIONS="Yes
-No"
-    PS3="Are the certificates self-signed?"
-    select TLS_SELF_SIGNED in ${OPTIONS}
-    do
-        case "${TLS_SELF_SIGNED}" in
-            "Yes")
-                break
-                ;;
-            "No")
-                break
-                ;;
-            *)
-                # Re-print the menu?
-                ;;
-        esac
-    done
-
-    PS3=${PS3_OLD}
-    IFS=${OLD_IFS}
-
-    if [ -n "${TLS_CACERT}" ]; then
-        if [ -n "${TLS_SERVER_PRIVATE_KEY_FILE}" ]; then
-            if [ -n "${TLS_SERVER_CERT_FILE}" ]; then
-                return 1
-            else
-                echo "Failed to get Server's Signed Key"
-                return 0
-            fi
-        else
-            echo "Failed to get Server's Private Key"
+    is_user_root
+    if [ $? -eq 1 ]; then
+        get_ldap_dc
+        if [ $? -eq 0 ]; then
             return 0
         fi
+
+        get_ldap_admin
+        if [ $? -eq 0 ]; then
+            return 0
+        fi
+
+        ACCESS_LDIF=`${MAKE_TEMP_FILE}`
+
+        echo "dn: olcDatabase={1}hdb,cn=config
+changetype: modify
+add: olcAccess
+olcAccess: {1}to attrs=loginShell,gecos
+  by dn=\"${LDAP_ADMIN},${LDAP_DC}\" write
+  by self write
+  by * read
+    " > "${ACCESS_LDIF}"
+
+        ${LDAP_MODIFY} -Y EXTERNAL -H ldapi:/// -f "${ACCESS_LDIF}"
+        local -i result=$?
+
+        ${RM} "${ACCESS_LDIF}"
+
+        return ${result}
     else
-        echo "Failed to get Certificate Authority's Key"
+        echo ${USER_MUST_BE_ROOT}
         return 0
     fi
     }
 
-function warn_user_config_update_ldaps()
+# Add better indexing of the LDAP
+function add_indexing()
     {
-    if [ -f /etc/debian_version]; then
-        echo
-        echo WARNING: You must enable LDAPS in the /etc/default/slapd file.
-        echo    To do so, change the SLAPD_SERVICES to specify LDAPS.
-        echo    For example:
-        echo
-        echo        SLAPD_SERVICES="ldap:/// ldapi:///"
-        echo
-        echo    becomes:
-        echo
-        echo        SLAPD_SERVICES="ldap://127.0.0.1:389/ ldaps:/// ldapi:///"
-        echo
-        echo    with non-TLS LDAP remaining available on the local-host
-        echo
+    is_user_root
+    if [ $? -eq 1 ]; then
+
+        INDEX_LDIF=`${MAKE_TEMP_FILE}`
+
+        echo "dn: olcDatabase={1}hdb,cn=config
+changetype: modify
+add: olcDbIndex
+olcDbIndex: cn pres,sub,eq
+-
+add: olcDbIndex
+olcDbIndex: sn pres,sub,eq
+-
+add: olcDbIndex
+olcDbIndex: uid pres,sub,eq
+-
+add: olcDbIndex
+olcDbIndex: displayName pres,sub,eq
+-
+add: olcDbIndex
+olcDbIndex: default sub
+-
+add: olcDbIndex
+olcDbIndex: uidNumber eq
+-
+add: olcDbIndex
+olcDbIndex: gidNumber eq
+-
+add: olcDbIndex
+olcDbIndex: mail,givenName eq,subinitial
+-
+add: olcDbIndex
+olcDbIndex: dc eq
+" > "${INDEX_LDIF}"
+
+        ${LDAP_MODIFY} -Y EXTERNAL -H ldapi:/// -f "${INDEX_LDIF}"
+        local -i result=$?
+
+        ${RM} "${INDEX_LDIF}"
+
+        return ${result}
+
+
+    else
+        echo ${USER_MUST_BE_ROOT}
+        return 0
     fi
     }
 
-# Configure LDAP to use LDAPS for secure inter-system communications
-function convert_to_ldaps()
+# Configure LDAP Server to use LDAPS for secure inter-system communications
+function convert_to_ldaps_server()
     {
     is_user_root
     if [ $? -eq 1 ]; then
@@ -254,7 +416,7 @@ olcTLSVerifyClient: never
             fi
 
             ${LDAP_MODIFY} -Y EXTERNAL -H ldapi:/// -f "${LDAPS_LDIF}"
-            let -i result=$?
+            local -i result=$?
 
             warn_user_config_update_ldaps
 
@@ -269,96 +431,81 @@ olcTLSVerifyClient: never
     fi
     }
 
-
-# Add access control
-function add_access_control()
+# Configure LDAP Client to use LDAPS for secure inter-system communications
+function configure_ldap_client()
     {
     is_user_root
     if [ $? -eq 1 ]; then
         get_ldap_dc
-        if [ $? -eq 0 ]; then
+        if [ $? -eq 1 ]; then
+
+            get_tls_certs true
+
+            if [ $? -eq 1 ]; then
+
+                get_ldap_server_address
+
+                if [ $? -eq 1 ]; then
+
+                    cp "${LDAP_BASE_DIR}/ldap.conf" "${LDAP_BASE_DIR}/ldap.conf.backup"
+
+                    sed -i "s/^TLS_CACERT/#OLD TLS_CACERT/" "${LDAP_BASE_DIR}/ldap.conf"
+                    echo "BASE  ${LDAP_DC}" >> "${LDAP_BASE_DIR}/ldap.conf"
+                    echo "URI   ldaps://${LDAP_SERVER_ADDRESS}/" >> "${LDAP_BASE_DIR}/ldap.conf"
+                    echo "TLS_CACERT    ${TLS_CACERT}" >> "${LDAP_BASE_DIR}/ldap.conf"
+                    return 1
+
+                else
+
+                    echo "Unable to get the required data for configuring the client"
+                    return 0
+
+                fi
+
+
+            else
+
+                echo "Unable to get the required data for configuring the client"
+                return 0
+
+            fi
+
+        else
+
+            echo "Unable to get the required data for configuring the client"
             return 0
+
         fi
-
-        get_ldap_admin
-        if [ $? -eq 0 ]; then
-            return 0
-        fi
-
-        ACCESS_LDIF=`${MAKE_TEMP_FILE}`
-
-        echo "dn: olcDatabase={1}hdb,cn=config
-changetype: modify
-add: olcAccess
-olcAccess: {1}to attrs=loginShell,gecos
-  by dn=\"${LDAP_ADMIN},${LDAP_DC}\" write
-  by self write
-  by * read
-    " > "${ACCESS_LDIF}"
-
-        ${LDAP_MODIFY} -Y EXTERNAL -H ldapi:/// -f "${ACCESS_LDIF}"
-        let -i result=$?
-
-        ${RM} "${ACCESS_LDIF}"
-
-        return ${result}
     else
         echo ${USER_MUST_BE_ROOT}
         return 0
     fi
     }
 
-
-# Add better indexing of the LDAP
-function add_indexing()
+# Configure LDAP Server with users, groups, LDAPS, etc.
+function configure_ldap_server()
     {
-    is_user_root
-    if [ $? -eq 1 ]; then
-        
-        INDEX_LDIF=`${MAKE_TEMP_FILE}`
-        
-        echo "dn: olcDatabase={1}hdb,cn=config
-changetype: modify
-add: olcDbIndex
-olcDbIndex: cn pres,sub,eq
--
-add: olcDbIndex
-olcDbIndex: sn pres,sub,eq
--
-add: olcDbIndex
-olcDbIndex: uid pres,sub,eq
--
-add: olcDbIndex
-olcDbIndex: displayName pres,sub,eq
--
-add: olcDbIndex
-olcDbIndex: default sub
--
-add: olcDbIndex
-olcDbIndex: uidNumber eq
--
-add: olcDbIndex
-olcDbIndex: gidNumber eq
--
-add: olcDbIndex
-olcDbIndex: mail,givenName eq,subinitial
--
-add: olcDbIndex
-olcDbIndex: dc eq
-" > "${INDEX_LDIF}"
-
-        ${LDAP_MODIFY} -Y EXTERNAL -H ldapi:/// -f "${INDEX_LDIF}"
-        let -i result=$?
-
-        ${RM} "${INDEX_LDIF}"
-
-        return ${result}
-
-
-    else
-        echo ${USER_MUST_BE_ROOT}
+    add_indexing
+    if [ $? -eq 0 ]; then
+        echo "Failed to add indexing"
         return 0
     fi
+
+    add_access_control
+    if [ $? -eq 0 ]; then
+        echo "Failed to add access control"
+        return 0
+    fi
+
+    convert_to_ldaps_server
+    if [ $? -eq 0 ]; then
+        echo "Failed to convert to LDAPS"
+        return 0
+    fi
+
+    echo "Restarting OpenLDAP to move from the insecure LDAP to the secure LDAPS"
+    /etc/init.d/slapd restart
+    return $?
     }
 
 function install_openldap_debian()
@@ -374,7 +521,6 @@ function install_openldap_debian()
     dpkg-reconfigure -plow slapd
     }
 
-
 # Install OpenLDAP
 function install_openldap()
     {
@@ -382,39 +528,20 @@ function install_openldap()
     is_user_root
     if [ $? -eq 1 ]; then
 
-        let -i result=0
+        local -i result=0
 
         # Distributions
         if [ -f /etc/debian_version ]; then
             echo "Detected Debian Distro..."
 
             install_openldap_debian
-            let -i result=$?
+            local -i result=$?
         fi
 
         # Check if the distribution successfully installed the tools we require
         if [ ${result} -eq 1 ]; then
-
-            add_indexing
-            if [ $? -eq 0 ]; then
-                echo "Failed to add indexing"
-                return 0
-            fi
-
-            add_access_control
-            if [ $? -eq 0 ]; then
-                echo "Failed to add access control"
-                return 0
-            fi
-
-            convert_to_ldaps
-            if [ $? -eq 0 ]; then
-                echo "Failed to convert to LDAPS"
-                return 0
-            fi
-
-            echo "Restarting OpenLDAP to move from the insecure LDAP to the secure LDAPS"
-            /etc/init.d/slapd restart
+            configure_ldap_server
+            return $?
         fi
     else
         echo ${USER_MUST_BE_ROOT}
@@ -425,13 +552,13 @@ function install_openldap()
 
 function main()
     {
-    PS3="Select option:"
+    PS3="Select option: "
     IFS='
 '
     ACTIONS="Install OpenLDAP
-LDAP: Add Access Control
-LDAP: Add Indexing
-LDAP: Convert to LDAPS
+Configure Client
+Configure Server
+Server: Convert to LDAPS
 Exit"
     select ACTION in ${ACTIONS}
     do
@@ -440,18 +567,16 @@ Exit"
                 install_openldap
                 ;;
 
-            "LDAP: Add Access Control")
-                # Works
-                add_access_control
+            "Configure Client")
+                configure_ldap_client
                 ;;
 
-            "LDAP: Add Indexing")
-                # Works
-                add_indexing
+            "Configure Server")
+                configure_ldap_server
                 ;;
 
-            "LDAP: Convert to LDAPS")
-                convert_to_ldaps
+            "Server: Convert to LDAPS")
+                convert_to_ldaps_server
                 if [ $? -eq 1 ]; then
                     echo "Convertion complete."
                     echo "Please remember to restart the LDAP Server and update all LDAP users to ldaps:// from ldapi://"
@@ -461,10 +586,6 @@ Exit"
             "Exit")
                 echo "Terminating"
                 break
-                ;;
-            
-            *)
-                # Re-print the menu?
                 ;;
         esac
     done
